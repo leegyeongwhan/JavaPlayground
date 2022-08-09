4424f447d512249f7f3942d0cbbedc37a61b2275
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-08-09 09:57:48 +0900

    feat: 로또 프로그램 맞는 숫자 갯수 체크 메서드 작성

--- src/MyLotto/Start.java
+++ src/MyLotto/Start.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Start {
     Random random = new Random();
-    int[] myNumber = new int[6];
-    int[] realNumber = new int[6];
+    static int[] myNumber = new int[6];
+    static int[] realNumber = new int[6];
 
     private void randomSetNumber(int[] realNumber) {
         for (int i = 0; i < realNumber.length; i++) {
@@ -30,10 +30,44 @@ public class Start {
         randomSetNumber(myNumber);
     }
 
+    //6 개 1등 5개 2등
 
     public static void main(String[] args) {
         Start start = new Start();
         start.setMyNumber();
+        start.setRealNumber();
         System.out.println("start = " + Arrays.toString(start.myNumber));
+        System.out.println("realNumber = " + Arrays.toString(start.realNumber));
+        System.out.println(checkNumber());
     }
+
+
+    //담청 확인 임의 담청배열과 내 배열과 비교 즉 두배열을 비교
+    private static String checkNumber() {
+        int checkCnt = 0;
+        //1등 배열
+        for (int i = 0; i < realNumber.length; i++) {
+            for (int j = 0; j < myNumber.length; j++) {
+                if (realNumber[i] == myNumber[j]) {
+                    checkCnt++;
+                }
+            }
+        }
+
+        switch (checkCnt) {
+            case 6:
+                return "1등";
+            case 5:
+                return "2등";
+            case 4:
+                return "3등";
+            case 3:
+                return "4등";
+            default:
+                return "꽝";
+        }
+    }
+
+    //당첨 회차 생성
+
 }
